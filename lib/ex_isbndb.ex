cb6a3b365b87ff17f2844f71ea6f708a6a76638c
xaviRodri <xavi.rm06@gmail.com>
2022-03-23 12:47:02 +0100

    Initial commit with Elixir project
    Adding main files for the Elixir project

---
+++ lib/ex_isbndb.ex
@@ -0,0 +1,18 @@
+defmodule ExIsbndb do
+  @moduledoc """
+  Documentation for `ExIsbndb`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExIsbndb.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_isbndb_test.exs
@@ -0,0 +1,8 @@
+defmodule ExIsbndbTest do
+  use ExUnit.Case
+  doctest ExIsbndb
+
+  test "greets the world" do
+    assert ExIsbndb.hello() == :world
+  end
+end
