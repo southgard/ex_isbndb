bc550e2304c57fc30cce30e2f0ca44531f0eb97e
xaviRodri <xavi.rm06@gmail.com>
2023-03-14 18:17:58 +0100

    Update version to 1.0.1

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule ExIsbndb.MixProject do
       deps: deps(),
 
       # Package
-      version: "1.0.0",
+      version: "1.0.1",
       description: description(),
       package: package(),
 
